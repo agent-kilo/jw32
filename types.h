bf9c1312ed1644d2b96273643780498c5d24db05
k <>
2023-09-19 10:37:24 +0000

    GetMessage(WIP)
    FossilOrigin-Name: ea9ac1b5bd2ca1b512a0e2dd03265b1008d2256c8e23c0c5e6abecabe317a985

--- types.h
+++ types.h
@@ -7,9 +7,21 @@
 #define jw32_wrap_handle(val) janet_wrap_pointer((void *)val)
 #define jw32_unwrap_handle(val) ((HANDLE)janet_unwrap_pointer(val))
 
-/* XXX: DWORD is uint32_t, but janet converts all numbers
-   into double float. use u64 here to preserve precision */
-#define jw32_wrap_dword(val) janet_wrap_u64((uint64_t)val)
-#define jw32_unwrap_dword(val) ((DWORD)janet_unwrap_u64(val))
+/* XXX: below are 32 bit data types, but janet converts all numbers
+   into double float, and doesn't have 32 bit integer types.
+   use 64 bit types here to preserve precision */
+
+/* DWORD: 32 bit unsigned */
+#define jw32_wrap_dword(x)    janet_wrap_u64((uint64_t)(x))
+#define jw32_unwrap_dword(x)  ((DWORD)janet_unwrap_u64(x))
+/* UINT: 32 bit unsigned */
+#define jw32_wrap_uint(x)     janet_wrap_u64((uint64_t)(x))
+#define jw32_unwrap_uint(x)   ((UINT)janet_unwrap_u64(x))
+/* WPARAM: 64 bit (on x64 machines) or 32 bit (on x86 machines) unsighed */
+#define jw32_wrap_wparam(x)   janet_wrap_u64((uint64_t)(x))
+#define jw32_unwrap_wparam(x) ((WPARAM)janet_unwrap_u64(x))
+/* LPARAM: 32 bit signed */
+#define jw32_wrap_lparam(x)   janet_wrap_s64((int64_t)(x))
+#define jw32_unwrap_lparam(x) ((LPARAM)janet_unwrap_s64(x))
 
 #endif
--- winuser.c
+++ winuser.c
@@ -9,6 +9,33 @@ static Janet cfun_GetDesktopWindow(int32_t argc, Janet *argv)
     return jw32_wrap_handle(GetDesktopWindow());
 }
 
+static Janet cfun_GetMessage(int32_t argc, Janet *argv)
+{
+    MSG msg;
+    BOOL ret = GetMessage(&msg);
+
+    if (ret != 0) {
+        JanetTable *msg_table;
+        JanetTuple *msg_point;
+
+        if (ret == -1) {
+            /* error */
+            janet_panicf("GetMessage failed: %" PRIu32 "\n", GetLastError());
+        }
+
+        msg_table = janet_table(7); /* the field count in MSG struct */
+        janet_table_put(msg_table, janet_wrap_keyword("hwnd"), jw32_wrap_handle(msg.hwnd));
+        janet_table_put(msg_table, janet_wrap_keyword("message"), jw32_wrap_uint(msg.message));
+        janet_table_put(msg_table, janet_wrap_keyword("wParam"), jw32_wrap_wparam(msg.wParam));
+        janet_table_put(msg_table, janet_wrap_keyword("lParam"), jw32_wrap_lparam(msg.lParam));
+        janet_table_put(msg_table, janet_wrap_keyword("time"), jw32_wrap_dword(msg.time));
+        janet_table_put(msg_table, janet_wrap_keyword("lPrivate"), jw32_wrap_dword(msg.lPrivate));
+    } else {
+        /* WM_QUIT received */
+        return janet_wrap_nil();
+    }
+}
+
 
 static const JanetReg cfuns[] = {
     {
