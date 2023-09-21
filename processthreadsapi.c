7934d5bec9dd27bfc3969c6f85f6d1dec271d3c4
k <>
2023-09-21 02:28:39 +0000

    processthreadsapi
    FossilOrigin-Name: ab1ea074c39bee76631075d4391413f7ebe8a839584e6ade1fe012803305de85

---
+++ processthreadsapi.c
@@ -0,0 +1,26 @@
+#include "processthreadsapi.h"
+
+#define MOD_NAME "processthreadsapi"
+
+
+static Janet cfun_GetCurrentThreadId(int32_t argc, Janet *argv)
+{
+    return jw32_wrap_dword(GetCurrentThreadId());
+}
+
+
+static const JanetReg cfuns[] = {
+    {
+        "GetCurrentThreadId",
+        cfun_GetCurrentThreadId,
+        "(" MOD_NAME "/GetCurrentThreadId)\n\n"
+        "Returns the current thread id.",
+    },
+    {NULL, NULL, NULL},
+};
+
+
+JANET_MODULE_ENTRY(JanetTable *env)
+{
+    janet_cfuns(env, MOD_NAME, cfuns);
+}
---
+++ processthreadsapi.h
@@ -0,0 +1,8 @@
+#ifndef __JW32_PROCESSTHREADSAPI_H
+#define __JW32_PROCESSTHREADSAPI_H
+
+#include <windows.h>
+#include <janet.h>
+#include "types.h"
+
+#endif
