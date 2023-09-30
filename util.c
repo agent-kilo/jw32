0b3e11209a66922d69e9591026cc0c54d0551f7d
k <>
2023-09-30 14:22:37 +0000

    utility functions
    FossilOrigin-Name: 64b0133a8a74172b9e79e97820e97957fb527ad37d164f5bb2ea8f88ee09761c

---
+++ util.c
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <janet.h>
+#include "types.h"
+
+#define MOD_NAME "util"
+
+
+static Janet cfun_IsNull(int32_t argc, Janet *argv)
+{
+    LPVOID p;
+    janet_fixarity(argc, 1);
+    p = jw32_get_lpvoid(argv, 0);
+    return janet_wrap_boolean(p == NULL);
+}
+
+
+static const JanetReg cfuns[] = {
+    {
+        "null?",
+        cfun_IsNull,
+        "(" MOD_NAME "/null? pointer)\n\n"
+        "Check if pointer is NULL.",
+    },
+    {NULL, NULL, NULL},
+};
+
+
+JANET_MODULE_ENTRY(JanetTable *env)
+{
+    janet_cfuns(env, MOD_NAME, cfuns);
+}
