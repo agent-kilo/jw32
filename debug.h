e364cdedacdc05717d46bbfe27dceea9b54b8a37
k <>
2023-09-30 16:30:11 +0000

    debug instruments
    FossilOrigin-Name: 2c1923a010e9223f97c24bc0e9c8b4e71753904cc1cc1244f9c8e2cd24fd7ba2

---
+++ debug.h
@@ -0,0 +1,47 @@
+#ifndef __JW32_DEBUG_H
+#define __JW32_DEBUG_H
+
+#include <stdio.h>
+#include <inttypes.h>
+#include <janet.h>
+
+#ifdef JW32_DEBUG
+
+#define jw32_dbg(fmt, ...)                                              \
+    do {                                                                \
+        fprintf(stderr, "-- %s:%s:%d: " fmt "\n", __FILE__, __func__, __LINE__, __VA_ARGS__); \
+        fflush(stderr);                                                 \
+    } while (0)
+
+#define jw32_dbg_msg(msg)                                               \
+    do {                                                                \
+        fprintf(stderr, "-- %s:%s:%d: " msg "\n", __FILE__, __func__, __LINE__); \
+        fflush(stderr);                                                 \
+    } while (0)
+
+#define jw32_dbg_val(val, fmt)                  \
+    jw32_dbg(#val " = " fmt, (val))
+
+#define jw32_dbg_jval(val, fmt)                                         \
+    do {                                                                \
+        janet_eprintf("-- %s:%s:%d: " #val " = " fmt "\n", __FILE__, __func__, __LINE__, (val)); \
+        fflush(stderr);                                                 \
+    } while (0)
+
+#else /* JW32_DEBUG */
+
+#define jw32_dbg(fmt, ...)                      \
+    do {} while (0)
+
+#define jw32_dbg_msg(msg)                       \
+    do {} while (0)
+
+#define jw32_dbg_val(val, fmt)                  \
+    do {} while (0)
+
+#define jw32_dbg_jval(val, fmt)                 \
+    do {} while (0)
+
+#endif /* JW32_DEBUG */
+
+#endif /* __JW32_DEBUG_H */
