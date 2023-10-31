f278b0b543d1b766be1fa3574379b888dbdb872c
k <>
2023-10-31 02:09:18 +0000

    error handling facility
    FossilOrigin-Name: 6cd8f9f59ece8c16194e4d2caf19458503350cad94e6571f12f03dbcacd7079f

--- jw32.h
+++ jw32.h
@@ -61,4 +61,19 @@ static inline Janet jw32_call_core_fn(const char *name, int argc, const Janet *a
     return janet_call(rfn, argc, argv);
 }
 
+static inline Janet jw32_hresult_errorv(HRESULT hr,
+                                        const char *file_name,
+                                        const char *cfun_name,
+                                        int32_t line_no)
+{
+    JanetTable *obj = janet_table(1);
+    janet_table_put(obj, janet_ckeywordv("hresult"), jw32_wrap_hresult(hr));
+    janet_table_put(obj, janet_ckeywordv("file"), janet_cstringv(file_name));
+    janet_table_put(obj, janet_ckeywordv("cfun"), janet_cstringv(cfun_name));
+    janet_table_put(obj, janet_ckeywordv("line"), janet_wrap_integer(line_no));
+    return janet_wrap_table(obj);
+}
+
+#define JW32_HRESULT_ERRORV(hr) jw32_hresult_errorv(hr, __FILE__, __func__, __LINE__)
+
 #endif /* __JW32_H */
