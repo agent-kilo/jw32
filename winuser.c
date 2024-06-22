40c22b5b46e427e6954bac9f78bd63484aa40de5
k <>
2024-06-22 19:53:35 +0000

    GetWindowRect
    FossilOrigin-Name: a38f12ac6152ddf6410b612ef3164202f0576597dc09aed9bf8650be3c522720

--- winuser.c
+++ winuser.c
@@ -3700,6 +3700,24 @@ static Janet cfun_SetWindowLongPtr(int32_t argc, Janet *argv)
     return jw32_wrap_long_ptr(SetWindowLongPtr(hWnd, nIndex, dwNewLong));
 }
 
+static Janet cfun_GetWindowRect(int32_t argc, Janet *argv)
+{
+    HWND hWnd;
+
+    RECT rect = {0, 0, 0, 0};
+    Janet ret_tuple[2];
+
+    janet_fixarity(argc, 1);
+
+    hWnd = jw32_get_handle(argv, 0);
+
+    ret_tuple[0] = jw32_wrap_bool(GetWindowRect(hWnd, &rect));
+    ret_tuple[1] = janet_wrap_struct(jw32_rect_to_struct(&rect));
+
+    return janet_wrap_tuple(janet_tuple_n(ret_tuple, 2));
+}
+
+
 static Janet cfun_SetForegroundWindow(int32_t argc, Janet *argv)
 {
     HWND hWnd;
@@ -5044,6 +5062,12 @@ static const JanetReg cfuns[] = {
         cfun_SetWindowLongPtr,
         "(" MOD_NAME "/SetWindowLongPtr hWnd nIndex dwNewLong)\n\n"
         "Changes an attribute of the specified window.",
+    },
+    {
+        "GetWindowRect",
+        cfun_GetWindowRect,
+        "(" MOD_NAME "/GetWindowRect hWnd)\n\n"
+        "Returns the dimensions of a window's bounding rectangle.",
     },
     {
         "SetForegroundWindow",
